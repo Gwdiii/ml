25526498c9685ddea7cad3169b342eda2ea21e03
gwdiii <georgedanforth@proton.me>
2023-06-13 15:13:49 -0500

    init commit

---
+++ C1_W1_Lab02.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('./deeplearning.mplstyle')
+
+#x_train is the input variable (size in 1000 sqft)
+#y_train is the target (price in 1000s of dollars)
+x_train = np.array([1.0, 2.0])
+y_train = np.array([300.0, 500.0])
+
+print(f'x_train = {x_train}')
+print(f'y_train = {y_train}')
+
+#m is the number of training examples
+m = x_train.shape[0]
+
+print(f'x_train.shape: {x_train.shape}')
+print(f'Number of training examples is: {m}')
+
+i = 0 #change this to 1 to see (x^(1), y^(1))
+
+x_i = x_train[i]
+y_i = y_train[i]
+print(f'(x^({i}), y^({i})) = ({x_i}, {y_i})')
+
+#plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r')
+# set the title
+plt.title('Housing Prices')
+# set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# set the x-axis label
+plt.xlabel('Size (in 1000s of sqft)')
+plt.show()
+
