ce7f3f7cfa8eab40b2bcaec72c9bb5b12abc948a
gwdiii <georgedanforth@proton.me>
2023-06-21 10:47:38 -0500

    completes C1_W2_Lab03.py

--- C1_W2_Lab03.py
+++ C1_W2_Lab03.py
@@ -28,6 +28,7 @@ def zscore_normalize_features(X):
 
     return (X_norm, mu, sigma)
 
+# set IO config options
 np.set_printoptions(precision=2)
 plt.style.use("./deeplearning.mplstyle")
 
@@ -83,3 +84,62 @@ ax[2].axis("equal")
 plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 fig.suptitle("distribution of features before, during, after normalization")
 plt.show()
+
+# normalize the original feature
+X_norm, X_mu, X_sigma = zscore_normalize_features(X_train)
+print(f"X_mu = {X_mu}, X_sigma = {X_sigma}")
+print(f"Peak to Peak range by column in Raw X: {np.ptp(X_train, axis=0)}")
+print(f"Peak to Peak range by column in Normalized X: {np.ptp(X_norm, axis=0)}")
+
+fig, ax = plt.subplots(1, 4, figsize=(12, 3))
+
+for i in range(len(ax)):
+    norm_plot(ax[i], X_train[:,i],)
+    ax[i].set_xlabel(X_features[i])
+
+ax[0].set_ylabel("count")
+fig.suptitle("distribution of features before normalization")
+plt.show()
+
+fig, ax = plt.subplots(1, 4, figsize=(12,3))
+
+for i in range(len(ax)):
+    norm_plot(ax[i], X_norm[:,i],)
+    ax[i].set_xlabel(X_features[i])
+
+ax[0].set_ylabel("count")
+fig.suptitle("distribution of features after normalization")
+plt.show()
+
+w_norm, b_norm, hist = run_gradient_descent(X_norm, y_train, 1000, 1e-1)
+
+# predict target using normalized features
+m = X_norm.shape[0]
+yp = np.zeros(m)
+
+for i in range(m): yp[i] = np.dot(X_norm[i], w_norm) + b_norm
+
+# plot predictions and targets versus original features
+fig, ax = plt.subplots(1, 4, figsize=(12, 3), sharey=True)
+
+for i in range(len(ax)):
+    ax[i].scatter(X_train[:,i], y_train, label="target")
+    ax[i].set_xlabel(X_features[i])
+    ax[i].scatter(X_train[:,i], yp, color=dlc["dlorange"], label="predict")
+
+ax[0].set_ylabel("Price")
+ax[0].legend()
+
+fig.suptitle("target versus prediction using z-score normalized model")
+plt.show()
+
+# normalize example
+x_house = np.array([1200, 3, 1, 40])
+x_house_norm = (x_house - X_mu) / X_sigma
+print(x_house_norm)
+
+# predict and print
+x_house_predict = np.dot(x_house_norm, w_norm) + b_norm
+print(f"predicted price of a house with 1200 sqft, 3 bedrooms, 1 floors, 40 years old = ${x_house_predict * 1000:0.0f}")
+
+plt_equal_scale(X_train, X_norm, y_train)
